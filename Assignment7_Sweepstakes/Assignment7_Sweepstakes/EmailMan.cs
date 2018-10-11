a234a8d2e8ec75f9fdc6ba16f1478feeefa3895c
esoemad5 <esoemad5@gmail.com>
2018-10-11 14:13:34 -0500

    Used try catch in EmailMan to avoid a crash when no e-mail is entered.

--- Assignment7_Sweepstakes/Assignment7_Sweepstakes/EmailMan.cs
+++ Assignment7_Sweepstakes/Assignment7_Sweepstakes/EmailMan.cs
@@ -14,21 +14,31 @@ namespace Assignment7_Sweepstakes
     {
         public static void SendWinnerMessageTo(string winnersEmail, string sweepstakesName, string firstName, string lastName)
         {
-            MailAddress sender = new MailAddress("ab6343416@gmail.com");
-            MailAddress recipient = new MailAddress(winnersEmail);
-            MailMessage message = new MailMessage(sender, recipient);
-            message.Subject = "Congration!";
-            message.Body = "Congrats " + firstName + " " + lastName + ", you won the: " + sweepstakesName + " sweepstakes!!!";
+            try
+            {
+                MailAddress sender = new MailAddress("ab6343416@gmail.com");
+                MailAddress recipient = new MailAddress(winnersEmail);
+                MailMessage message = new MailMessage(sender, recipient);
+                message.Subject = "Congration!";
+                message.Body = "Congrats " + firstName + " " + lastName + ", you won the: " + sweepstakesName + " sweepstakes!!!";
 
-            SmtpClient client = new SmtpClient();
-            client.Port = 587;
-            client.Host = "smtp.gmail.com";
-            client.EnableSsl = true;
-            client.Timeout = 10000;
-            client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            client.UseDefaultCredentials = false;
-            client.Credentials = new System.Net.NetworkCredential("ab6343416@gmail.com", "HorseBatteryStaple");
-            client.Send(message);
+                SmtpClient client = new SmtpClient();
+                client.Port = 587;
+                client.Host = "smtp.gmail.com";
+                client.EnableSsl = true;
+                client.Timeout = 10000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential("ab6343416@gmail.com", "HorseBatteryStaple");
+                client.Send(message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("An exception happened with e-mail delivery. It is likely that no e-mail was entered for the winner.");
+                Console.WriteLine("The program will continue without sending the winner an e-mail.");
+                //Console.WriteLine(e.Message);
+            }
+            
         }
     }
 }
